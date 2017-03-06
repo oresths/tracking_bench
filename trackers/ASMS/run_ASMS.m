d02192dd245aae8e7127824a59836781e9bfcac8
oresths <orestis@ee.auth.gr>
2017-03-06 17:27:03 +0000

    Added ASMS

---
+++ trackers/ASMS/run_ASMS.m
@@ -0,0 +1,24 @@
+function results = run_ASMS(seq, res_path, bSaveImage)
+
+dlmwrite('region.txt', seq.init_rect);
+
+fid = fopen('images.txt','wt');
+[rows,~] = size(seq.s_frames);
+for i=1:rows-1
+      fprintf(fid,'%s,',seq.s_frames{i,1:end-1});
+      fprintf(fid,'%s\n',seq.s_frames{i,end});
+end
+fprintf(fid,'%s,',seq.s_frames{rows, 1:end-1});
+fprintf(fid,'%s\n',seq.s_frames{rows, end});
+fclose(fid);
+
+tic
+tracker_command = '.\asms_vot.exe';
+dos(tracker_command);
+duration=toc;
+
+results.res = dlmread('output.txt');
+results.type='rect';
+results.fps=seq.len / duration;
+
+disp(results.fps)
